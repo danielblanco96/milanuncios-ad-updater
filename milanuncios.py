c5e17d1ad9307d8426eabd5d82c3d841d14ec0ff
Daniel Blanco <dablancal96@gmail.com>
2021-03-06 15:05:44 +0100

    Refactor code and add json config

--- milanuncios.py
+++ milanuncios.py
@@ -1,10 +1,25 @@
-import argparse
+import time
+import json
+import yagmail
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+class LoginException:
+    pass
+
+class NoAdsFoundException:
+    pass
+
+class EmailSender:
+    def __init__(self):
+        super().__init__()
+
+    def send_email(self, to, message):
+        print("done")
 
 class MilanunciosScrapper:
     def __init__(self, chrome_opts):
@@ -37,28 +52,34 @@ class MilanunciosScrapper:
         )
 
         ok_button.click()
-
+        
+    def update_ads(self):
         WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "ul.ma-AdButtonBarMyAds li:nth-child(2)"))
         )
         
         update_ad_button_list = self.driver.find_elements_by_css_selector("ul.ma-AdButtonBarMyAds li:nth-child(2)")
+        if(not update_ad_button_list):
+            raise NoAdsFoundException("No se encontraron anuncios en tu cuenta.")
 
         for button in update_ad_button_list:
             button.click()
         
-        
 
-parser = argparse.ArgumentParser("milanuncios")
-parser.add_argument("email", help="Milanuncios account email")
-parser.add_argument("password", help="Milanuncios account password")
-args= parser.parse_args()
+def main():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--disable-gpu')
-milanuncios_scrapper = MilanunciosScrapper(None);
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    milanuncios_scrapper = MilanunciosScrapper(None)
 
-milanuncios_scrapper.accept_cookies_if_exist()
-milanuncios_scrapper.login(args.email, args.password)
-milanuncios_scrapper.close_popup_if_exist()
+    milanuncios_scrapper.accept_cookies_if_exist()
+    milanuncios_scrapper.login(config['milanuncios']['email'], config['milanuncios']['password'])
+    milanuncios_scrapper.close_popup_if_exist()
+    milanuncios_scrapper.update_ads()
+    time.sleep(10)
+    
+if __name__ == "__main__":
+    main()
